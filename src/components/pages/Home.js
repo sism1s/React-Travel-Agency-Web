27d5b603520cd05bee9050ffa780d8a16c61797f
sism1s <marko.lazic880@gmail.com>
2021-02-28 22:37:36 +0000

    Added card components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
 import HeroSection from "../HeroSection/HeroSection";
+import Cards from "../CardItem/Cards";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
